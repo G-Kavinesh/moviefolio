80d9f7ee4ec046c296b197f81032383dc5745187
Kavin <indranigk@gmail.com>
2024-04-20 09:52:31 +0530

    Commit-2

---
+++ MovieFolio/app/src/main/java/com/example/bookappfinal/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.bookappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Intent;
+import android.support.v4.app.RemoteActionCompatParcelizer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    private RequestQueue requestQueue;
+    private List<Movie> movieList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intToMain);
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
+
+        movieList = new ArrayList<>();
+        fetchMovies();
+
+
+    }
+
+    private void fetchMovies() {
+
+        String url = "https://k18avin.github.io/MovieList/db.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i = 0 ; i < response.length() ; i ++){
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String title = jsonObject.getString("title");
+                                String overview = jsonObject.getString("overview");
+                                String poster = jsonObject.getString("poster");
+                                Double rating = jsonObject.getDouble("rating");
+
+                                Movie movie = new Movie(title , poster , overview , rating);
+                                movieList.add(movie);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            MovieAdapter adapter = new MovieAdapter(MainActivity.this , movieList);
+
+                            recyclerView.setAdapter(adapter);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
+    }
+}
